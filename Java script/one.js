0edfc4af589203399f9addd339638cdb4e89c524
Kukkapalli <yaswanthkukkapalli630@gmail.com>
2025-08-22 10:26:43 +0530

    Bakery website with responsive layout and slideshow

---
+++ Java script/one.js
@@ -0,0 +1,10 @@
+
+let score = 100;
+console.log("Score:", score);
+let playerName = "John Doe";
+console.log("Player Name:", playerName);
+if (score > 50) {
+    console.log(playerName + " has a high score!");
+}   else {
+    console.log(playerName + " needs to improve.");
+}   
